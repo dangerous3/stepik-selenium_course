9169bb5e5e492d4028ab8ef8703ac502b4b06d57
dangerous3 <dangerous3@yandex.ru>
2020-01-14 23:40:04 +0300

    Добавлены решения заданий первого модуля и два решения задач из второго модуля

---
+++ lesson2.1_step5.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import math
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/math.html")
+
+    x = browser.find_element_by_id("input_value")
+    y = calc(x.text)
+
+    x_element = browser.find_element_by_id("answer")
+    x_element.send_keys(y)
+
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
